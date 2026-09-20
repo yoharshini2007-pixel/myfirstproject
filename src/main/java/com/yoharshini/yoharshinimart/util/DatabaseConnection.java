0075379bf73712d4b6e0c01c93fc3c1f2ee96a8f
yoharshini <yoharshini2007@gmail.com>
2026-09-20 16:42:01 +0530

    Configure database connection

--- src/main/java/com/yoharshini/yoharshinimart/util/DatabaseConnection.java
+++ src/main/java/com/yoharshini/yoharshinimart/util/DatabaseConnection.java
@@ -1,21 +1,26 @@
+package com.yoharshini.yoharshinimart.util;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/my_marketplace";
-    private static final String USER = "root";
-    private static final String PASSWORD = "";
+    private static final String URL = "jdbc:mysql://localhost:3306/yoharshinimart";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "yoha@2026";
 
     public static Connection getConnection() {
-        Connection conn = null;
+        Connection con = null;
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            conn = DriverManager.getConnection(URL, USER, PASSWORD);
-            System.out.println("Database Connected Successfully!");
-        } catch (ClassNotFoundException | SQLException e) {
+            con = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (ClassNotFoundException e) {
+            System.out.println("MySQL JDBC Driver not found!");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("Database Connection Failed!");
             e.printStackTrace();
         }
-        return conn;
+        return con;
     }
-} 
+}
