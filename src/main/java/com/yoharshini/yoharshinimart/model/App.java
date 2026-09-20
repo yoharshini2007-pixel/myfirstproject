662dfaca09775ebaf4a480b632adb24836295910
yoharshini <yoharshini2007@gmail.com>
2026-09-20 17:40:09 +0530

    Update application model

--- src/main/java/com/yoharshini/yoharshinimart/model/App.java
+++ src/main/java/com/yoharshini/yoharshinimart/model/App.java
@@ -1,5 +1,7 @@
+package com.yoharshini.yoharshinimart.model;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
-}
+} 
